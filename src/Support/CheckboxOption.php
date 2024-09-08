749f79ed93b2e1f6716bfffc630e31b396c4b2c6
Piotr Skrobol <taboritis@gmail.com>
2024-09-08 21:05:21 +0200

    WIP

--- src/Support/CheckboxOption.php
+++ src/Support/CheckboxOption.php
@@ -4,24 +4,30 @@ declare(strict_types=1);
 
 namespace Taboritis\ElegantSlack\Support;
 
+use JetBrains\PhpStorm\ArrayShape;
 use JsonSerializable;
 use Taboritis\ElegantSlack\Blocks\Section\Mrkdwn;
 use Taboritis\ElegantSlack\Blocks\Section\PlainText;
 
 class CheckboxOption implements JsonSerializable
 {
-    private PlainText|Mrkdwn|string $text;
-    private PlainText|Mrkdwn|string $description;
+    private PlainText|Mrkdwn $text;
+    private PlainText|Mrkdwn $description;
 
     public function __construct(
         PlainText|Mrkdwn|string $text,
         PlainText|Mrkdwn|string $description,
         private readonly string $value
-    ) {
+    )
+    {
         $this->text = is_string($text) ? new PlainText($text) : $text;
         $this->description = is_string($description) ? new PlainText($description) : $description;
     }
 
+
+    /**
+     * @return array<string, mixed>
+     */
     public function jsonSerialize(): array
     {
         return [
