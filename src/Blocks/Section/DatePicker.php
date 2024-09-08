abd4d44dc6fba7341dc3d7574a866650f5d7bdd2
Piotr Skrobol <taboritis@gmail.com>
2024-09-08 20:04:04 +0200

    WIP

--- src/Blocks/Section/DatePicker.php
+++ src/Blocks/Section/DatePicker.php
@@ -4,6 +4,35 @@ declare(strict_types=1);
 
 namespace Taboritis\ElegantSlack\Blocks\Section;
 
-class DatePicker
+use PHP_CodeSniffer\Generators\Markdown;
+use Taboritis\ElegantSlack\Blocks\Block;
+
+class DatePicker extends Block
 {
+    private PlainText|Markdown $text;
+    private PlainText $placeholder;
+
+    public function __construct(
+        PlainText|Markdown|string $text,
+        private readonly ?string $initialDate = null,
+        PlainText|string $placeholder,
+        private readonly string $actionId
+    ) {
+        $this->text = is_string($text) ? new PlainText($text) : $text;
+        $this->placeholder = is_string($placeholder) ? new PlainText($placeholder) : $placeholder;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return array(
+            'type' => 'section',
+            'text' => $this->text->jsonSerialize()['text'],
+            'accessory' => [
+                'type' => 'datepicker',
+                'initial_date' => $this->initialDate,
+                'placeholder' => $this->placeholder->jsonSerialize()['text'],
+                'action_id' => $this->actionId,
+            ]
+        );
+    }
 }
--- tests/Support/CheckboxOptionTest.php
+++ tests/Support/CheckboxOptionTest.php
@@ -18,4 +18,14 @@ class CheckboxOptionTest extends TestCase
         $option = new CheckboxOption('text', 'description', 'value');
         $this->assertInstanceOf(\JsonSerializable::class, $option);
     }
+
+    #[Test]
+    public function it_can_be_converted_to_array(): void
+    {
+        $option = new CheckboxOption('text', 'description', 'value');
+
+        $this->assertArrayHasKey('text', $option->jsonSerialize());
+        $this->assertArrayHasKey('description', $option->jsonSerialize());
+        $this->assertArrayHasKey('value', $option->jsonSerialize());
+    }
 }
