b0213ab76631b83690c78e0b5ee6f8cda46e99e3
Piotr Skrobol <taboritis@gmail.com>
2024-09-08 23:24:03 +0200

    WIP

--- src/Blocks/Section/SlackImage.php
+++ src/Blocks/Section/SlackImage.php
@@ -4,6 +4,34 @@ declare(strict_types=1);
 
 namespace Taboritis\ElegantSlack\Blocks\Section;
 
-class SlackImage
+use Taboritis\ElegantSlack\Blocks\Block;
+use Taboritis\ElegantSlack\Support\Mrkdwn;
+use Taboritis\ElegantSlack\Support\PlainText;
+
+class SlackImage extends Block
 {
+    private PlainText|Mrkdwn $text;
+
+    public function __construct(
+        PlainText|Mrkdwn|string $text,
+        private string $imageUrl,
+        private string $altText
+    ) {
+        $this->text = is_string($text) ? new PlainText($text) : $text;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'type' => 'section',
+            'text' => $this->text->jsonSerialize(),
+            'accessory' => [
+                'type' => 'image',
+                'slack_file' => [
+                    'url' => $this->imageUrl,
+                ],
+                'alt_text' => $this->altText,
+            ],
+        ];
+    }
 }
--- tests/Blocks/Section/SlackImageTest.php
+++ tests/Blocks/Section/SlackImageTest.php
@@ -4,16 +4,37 @@ declare(strict_types=1);
 
 namespace Tests\Blocks\Section;
 
-use Taboritis\ElegantSlack\Blocks\Section\SlackImage;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Taboritis\ElegantSlack\Blocks\Block;
+use Taboritis\ElegantSlack\Blocks\Section\SlackImage;
 
 #[CoversClass(SlackImage::class)]
-class SlackImageTest extends \PHPUnit\Framework\TestCase
+class SlackImageTest extends TestCase
 {
     #[Test]
-    public function it_(): void
+    public function it_extends_a_block(): void
     {
-        $this->markTestIncomplete("TODO: " . __METHOD__);
+        $reflection = new \ReflectionClass(SlackImage::class);
+        $this->assertTrue($reflection->isSubclassOf(Block::class));
+    }
+
+    #[Test]
+    public function it_can_be_converted_to_array(): void
+    {
+        $section = new SlackImage(
+            text: 'Hello, world!',
+            imageUrl: 'https://example.com/image.jpg',
+            altText: 'alt text'
+        );
+
+        $this->assertArrayHasKey('type', $section->jsonSerialize());
+        $this->assertArrayHasKey('text', $section->jsonSerialize());
+        $this->assertArrayHasKey('accessory', $section->jsonSerialize());
+        $this->assertArrayHasKey('type', $section->jsonSerialize()['accessory']);
+        $this->assertArrayHasKey('slack_file', $section->jsonSerialize()['accessory']);
+        $this->assertArrayHasKey('url', $section->jsonSerialize()['accessory']['slack_file']);
+        $this->assertArrayHasKey('alt_text', $section->jsonSerialize()['accessory']);
     }
 }
