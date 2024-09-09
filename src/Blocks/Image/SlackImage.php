693d27c5c8a8a4df5e8ddfc6e1d60f4f0cdce32d
Piotr Skrobol <taboritis@gmail.com>
2024-09-09 20:12:39 +0200

    Add image serialization methods to Image classes

--- src/Blocks/Image/SlackImage.php
+++ src/Blocks/Image/SlackImage.php
@@ -4,6 +4,22 @@ declare(strict_types=1);
 
 namespace Taboritis\ElegantSlack\Blocks\Image;
 
-class SlackImage
+use Taboritis\ElegantSlack\Blocks\Block;
+
+class SlackImage extends Block
 {
+    public function __construct(private string $url, private string $altText)
+    {
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'type' => 'image',
+            'slack_file' => [
+                'url' => $this->url,
+            ],
+            'alt_text' => $this->altText,
+        ];
+    }
 }
--- tests/Blocks/Image/SlackImageTest.php
+++ tests/Blocks/Image/SlackImageTest.php
@@ -4,17 +4,37 @@ declare(strict_types=1);
 
 namespace Tests\Blocks\Image;
 
-use Taboritis\ElegantSlack\Blocks\Image\SlackImage;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
+use Taboritis\ElegantSlack\Blocks\Image\SlackImage;
 
 #[CoversClass(SlackImage::class)]
 class SlackImageTest extends TestCase
 {
     #[Test]
-    public function it_(): void
+    public function it_extends_a_block(): void
     {
-        $this->markTestIncomplete("TODO: " . __METHOD__);
+        $reflection = new \ReflectionClass(SlackImage::class);
+
+        $this->assertTrue($reflection->isSubclassOf(\Taboritis\ElegantSlack\Blocks\Block::class));
+    }
+
+    #[Test]
+    public function it_can_be_converted_to_array(): void
+    {
+        $url = 'https://example.com/image.png';
+        $altText = 'Example Image';
+        $slackImage = new SlackImage($url, $altText);
+
+        $expected = [
+            'type' => 'image',
+            'slack_file' => [
+                'url' => $url,
+            ],
+            'alt_text' => $altText,
+        ];
+
+        $this->assertSame($expected, $slackImage->jsonSerialize());
     }
 }
