0882bf3cb1493aee4a7c1e353033c892ab248ec4
Piotr Skrobol <taboritis@gmail.com>
2024-09-09 19:07:00 +0200

    Add jsonSerialize method to Divider class

--- src/Blocks/Divider/Divider.php
+++ src/Blocks/Divider/Divider.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace Taboritis\ElegantSlack\Blocks\Divider;
 
-class Divider
+use Taboritis\ElegantSlack\Blocks\Block;
+
+class Divider extends Block
 {
+    public function jsonSerialize(): array
+    {
+        return [
+            'type' => 'divider',
+        ];
+    }
 }
