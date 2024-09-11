b9623de7f28677a3414ec6aa35a0c9d2c02dc323
Piotr Skrobol <taboritis@gmail.com>
2024-09-11 20:08:14 +0200

    MultilinePlainTextInput

--- src/Blocks/Input/MultilinePlainTextInput.php
+++ src/Blocks/Input/MultilinePlainTextInput.php
@@ -4,6 +4,28 @@ declare(strict_types=1);
 
 namespace Taboritis\ElegantSlack\Blocks\Input;
 
-class MultilinePlainTextInput
+use Taboritis\ElegantSlack\Blocks\Block;
+use Taboritis\ElegantSlack\Support\PlainText;
+
+class MultilinePlainTextInput extends Block
 {
+    private PlainText $label;
+
+    public function __construct(PlainText|string $label, private readonly string $actionId)
+    {
+        $this->label = is_string($label) ? new PlainText($label) : $label;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'type' => 'input',
+            'element' => [
+                'type' => 'plain_text_input',
+                'multiline' => true,
+                'action_id' => $this->actionId,
+            ],
+            'label' => $this->label->jsonSerialize()
+        ];
+    }
 }
--- tests/Blocks/Input/MultilinePlainTextInputTest.php
+++ tests/Blocks/Input/MultilinePlainTextInputTest.php
@@ -4,17 +4,34 @@ declare(strict_types=1);
 
 namespace Tests\Blocks\Input;
 
-use Taboritis\ElegantSlack\Blocks\Input\MultilinePlainTextInput;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
+use Taboritis\ElegantSlack\Blocks\Block;
+use Taboritis\ElegantSlack\Blocks\Input\MultilinePlainTextInput;
 
 #[CoversClass(MultilinePlainTextInput::class)]
 class MultilinePlainTextInputTest extends TestCase
 {
     #[Test]
-    public function it_(): void
+    public function it_extends_a_block(): void
     {
-        $this->markTestIncomplete("TODO: " . __METHOD__);
+        $reflection = new \ReflectionClass(MultilinePlainTextInput::class);
+        $this->assertTrue($reflection->isSubclassOf(Block::class));
+    }
+
+    #[Test]
+    public function it_can_be_converted_to_array(): void
+    {
+        $block = new MultilinePlainTextInput('Label', 'action-id');
+
+        $result = $block->jsonSerialize();
+
+        $this->assertArrayHasKey('type', $result);
+        $this->assertArrayHasKey('element', $result);
+        $this->assertArrayHasKey('type', $result['element']);
+        $this->assertArrayHasKey('multiline', $result['element']);
+        $this->assertArrayHasKey('action_id', $result['element']);
+        $this->assertArrayHasKey('label', $result);
     }
 }
