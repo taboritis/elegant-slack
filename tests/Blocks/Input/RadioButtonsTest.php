631608b7fcc344d379ad97baeaaf973c9b3c9319
Piotr Skrobol <taboritis@gmail.com>
2024-09-13 20:00:56 +0200

    Add RadioButtons class for radio buttons input block

--- tests/Blocks/Input/RadioButtonsTest.php
+++ tests/Blocks/Input/RadioButtonsTest.php
@@ -4,17 +4,31 @@ declare(strict_types=1);
 
 namespace Tests\Blocks\Input;
 
-use Taboritis\ElegantSlack\Blocks\Input\RadioButtons;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
+use Taboritis\ElegantSlack\Blocks\Block;
+use Taboritis\ElegantSlack\Blocks\Input\RadioButtons;
 
 #[CoversClass(RadioButtons::class)]
 class RadioButtonsTest extends TestCase
 {
     #[Test]
-    public function it_(): void
+    public function it_extends_a_block(): void
     {
-        $this->markTestIncomplete("TODO: " . __METHOD__);
+        $reflection = new \ReflectionClass(RadioButtons::class);
+        $this->assertTrue($reflection->isSubclassOf(Block::class));
+    }
+
+    #[Test]
+    public function assert_that_array_has_keys(): void
+    {
+        $radioButtons = new RadioButtons('label', 'actionId');
+        $result = $radioButtons->jsonSerialize();
+
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('type', $result);
+        $this->assertArrayHasKey('element', $result);
+        $this->assertArrayHasKey('label', $result);
     }
 }
